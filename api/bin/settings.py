435ba9cbac769e967df3474a922c167989c2c9a7
ivan-tana <ivan8tana@gmail.com>
2022-08-11 12:08:11 +0100

    ignore files

--- api/bin/settings.py
+++ api/bin/settings.py
@@ -1,6 +1,6 @@
 
 
 SECRET_KEY = "This a test secret key"
-SQLALCHEMY_DATABASE_URI = "sqlite:///database.sqlite3"
+SQLALCHEMY_DATABASE_URI = "sqlite:///db.sqlite3"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
