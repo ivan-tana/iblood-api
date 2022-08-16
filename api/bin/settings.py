30175ebb644535a0193f0f3d68c873ffc89b1be8
ivan-tana <ivan8tana@gmail.com>
2022-08-16 14:44:19 +0100

    dot env python extension

--- api/bin/settings.py
+++ api/bin/settings.py
@@ -1,6 +1,6 @@
+import os
 
-
-SECRET_KEY = "This a test secret key"
-SQLALCHEMY_DATABASE_URI = "sqlite:///db.sqlite3"
+SECRET_KEY = os.environ.get("SECRET_KEY")
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
